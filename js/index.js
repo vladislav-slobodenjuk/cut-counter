b35e7e2feb2b7e282f460d2b195bb29d2ba47aa2
vladislav-slobodenjuk <vladjun@gmail.com>
2023-04-24 00:23:27 +0300

    import for products; autocomplete off

--- js/index.js
+++ js/index.js
@@ -1,35 +1,10 @@
+import { PRODUCTS } from "./products.js";
+// console.log(PRODUCTS);
+
 const refs = {
   productForm: document.querySelector(".product-form"),
   // productSelect: document.querySelector(".product"),
   output: document.querySelector(".output"),
-};
-
-const PRODUCTS = {
-  hartBlue: {
-    top: 440,
-    bottom: 220,
-    atBox: 18,
-    onPallet: 64,
-  },
-  bars_16: {
-    top: 150,
-    bottom: 0,
-    atBox: 12,
-    onPallet: 64,
-  },
-  guliver_20: {
-    top: 240,
-    bottom: 20,
-    atBox: 20,
-    onPallet: 120,
-  },
-  nightMix: {
-    top: 300,
-    bottom: 300,
-    correx: 450,
-    atBox: 9,
-    onPallet: 50,
-  },
 };
 
 function countProductCut(product, BoxQuantity) {
---
+++ js/products.js
@@ -0,0 +1,27 @@
+export const PRODUCTS = {
+  hartBlue: {
+    top: 440,
+    bottom: 220,
+    atBox: 18,
+    onPallet: 64,
+  },
+  bars_16: {
+    top: 150,
+    bottom: 0,
+    atBox: 12,
+    onPallet: 64,
+  },
+  guliver_20: {
+    top: 240,
+    bottom: 20,
+    atBox: 20,
+    onPallet: 120,
+  },
+  nightMix: {
+    top: 300,
+    bottom: 300,
+    correx: 450,
+    atBox: 9,
+    onPallet: 50,
+  },
+};
